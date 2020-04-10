35d64670802698f0e6fd06bafa6a4beda4c93d1c
Vassilis <bzafiris@gmail.com>
2020-04-10 19:03:48 +0300

    Renamed to team5 app

--- android/app/src/androidTest/java/com/example/parking/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/parking/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.parking;
 
 import android.content.Context;
 
--- android/app/src/test/java/com/example/parking/ExampleUnitTest.java
+++ android/app/src/test/java/com/example/parking/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.parking;
 
 import org.junit.Test;
 
