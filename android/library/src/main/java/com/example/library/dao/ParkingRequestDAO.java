dfc3f3228f3e1c8ce561119c5e86da5393d2056c
GerasimosLap <gerasimoslapakis@gmail.com>
2020-04-22 19:01:21 +0300

    DAO PR and User

--- android/library/src/main/java/com/example/library/dao/ParkingRequestDAO.java
+++ android/library/src/main/java/com/example/library/dao/ParkingRequestDAO.java
@@ -1,6 +1,12 @@
 package com.example.library.dao;
 
 import com.example.library.domain.ParkingRequest;
+import com.example.library.domain.ParkingSpace;
+import com.example.library.util.Pin;
+
+import java.util.ArrayList;
 
 public interface ParkingRequestDAO {
+    public ArrayList<ParkingSpace> findParking(ArrayList<ParkingSpace> parkingSpaces, int difference);//Βρίσκει parking σύμφωνα με τον Τ.Κ. Αν δεν υπάρχει κοντά ψάχνει τους κοντινούς Τ.Κ.
+    public boolean validateParking(Pin pin);
 }
--- android/library/src/main/java/com/example/library/dao/UserDAO.java
+++ android/library/src/main/java/com/example/library/dao/UserDAO.java
@@ -1,6 +1,13 @@
 package com.example.library.dao;
 
 import com.example.library.domain.User;
+import com.example.library.domain.Vehicle;
 
 public interface UserDAO {
+    public void addVehicle(Vehicle vehicle);//Προσθέτει ενα όχημα αν δεν υπάρχει ήδη στον χρήστη.
+
+    public void removeVehicle(Vehicle vehicle);//Αφαιρειί ένα όχημα (μόνο αν ο χρήστης έχει 2 και πάνω.
+
+    public double calculateRating();//Υπολογίζει τον μέσο όρο αξιολογήσεων
+
 }
