e5e50d51d1fc1842ab438241f042a65399ba089a
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-04-22 19:49:11 +0300

    dao change

--- android/library/src/main/java/com/example/library/dao/ParkingRequestDAO.java
+++ android/library/src/main/java/com/example/library/dao/ParkingRequestDAO.java
@@ -8,20 +8,9 @@ import java.util.ArrayList;
 
 public interface ParkingRequestDAO {
 
-    /**
-     * Βρίσκει parking σύμφωνα με τον Τ.Κ. Αν δεν υπάρχει κοντά ψάχνει τους κοντινούς Τ.Κ.
-     * @param
-     * @param difference
-     * @return Ο κατάλογος των αντικειμένων
-     */
+
     ArrayList<ParkingSpace> findParking(ArrayList<ParkingSpace> parkingSpaces, int difference);
 
-    /**
-     * Είναι υπεύθυνο για την διεκπεραίωση της διαδικασίας ανταλλαγής θέσης
-     * καθώς και την μεταφορά μονάδων από τον ενδιαφερόμενο στον
-     * σταθμευμένο χρήστη.
-     * @param pin
-     * @return Επιστρέφει true εάν η διαδικασία ανταλλαγής έχει ολοκληρωθεί με επιτυχία
-     */
+
     boolean validateParking(Pin pin);
 }
