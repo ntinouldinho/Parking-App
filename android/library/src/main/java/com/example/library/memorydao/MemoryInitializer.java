ccb78b6fc6e4b71a66095cd71fb15ba42a64352f
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-04-23 14:25:10 +0300

    erase data

--- android/library/src/main/java/com/example/library/memorydao/MemoryInitializer.java
+++ android/library/src/main/java/com/example/library/memorydao/MemoryInitializer.java
@@ -4,19 +4,38 @@ import com.example.library.dao.Initializer;
 import com.example.library.dao.ParkingRequestDAO;
 import com.example.library.dao.ParkingSpaceDAO;
 import com.example.library.dao.UserDAO;
+import com.example.library.domain.ParkingRequest;
+import com.example.library.domain.ParkingSpace;
+import com.example.library.domain.User;
+
+import java.util.List;
 
 public class MemoryInitializer extends Initializer {
 
     @Override
     protected void eraseData() {
+        List<User> users = getUserDAO().findAll();
+        for(User user : users) {
+            getUserDAO().delete(user);
+        }
+
+        List<ParkingRequest> parkingRequests = getRequestDAO().findAll();
+        for(ParkingRequest parkingRequest : parkingRequests) {
+            getRequestDAO().delete(parkingRequest);
+        }
+
+        List<ParkingSpace> parkingSpaces = getParkingDAO().findAll();
+        for(ParkingSpace parkingSpace : parkingSpaces) {
+            getParkingDAO().delete(parkingSpace);
+        }
     }
 
-
+    @Override
     public void prepareData(){
-    eraseData();
+        eraseData();
 
 
-}
+    }
     @Override
     protected ParkingRequestDAO getRequestDAO() {
         return new ParkingRequestDAOMemory();
