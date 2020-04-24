1c07f7d426f6690290943b18342e8a5e64eedf04
GerasimosLap <gerasimoslapakis@gmail.com>
2020-04-24 18:39:53 +0300

    doublez

--- android/app/src/main/java/com/example/parking/test/CreditsTest.java
+++ android/app/src/main/java/com/example/parking/test/CreditsTest.java
@@ -10,26 +10,22 @@ public class CreditsTest {
     public void addCreditsTest(){
         Credits c = new Credits();
         c.addCredits(10);
-        Assert.assertEquals(10,c.getPoints());
+        Assert.assertEquals(10.0,c.getPoints());
     }
 
     @Test
-<<<<<<< HEAD
     public void removeValidCreditsTest(){
         Credits c = new Credits();
         c.addCredits(10);
         c.removeCredits(5);
-        Assert.assertEquals(5,c.getPoints());
+        Assert.assertEquals(5.0,c.getPoints());
     }
-=======
-    public void removeCreditsTest(){
->>>>>>> 6c445e1007a105bf6c6199b614c04e82db0d5a67
 
     @Test
     public void removeInvalidCreditsTest(){
         Credits c = new Credits();
         c.addCredits(10);
-        c.removeCredits(11);;
-        Assert.assertEquals(10,c.getPoints());
+        c.removeCredits(11);
+        Assert.assertEquals(10.0,c.getPoints());
     }
 }
