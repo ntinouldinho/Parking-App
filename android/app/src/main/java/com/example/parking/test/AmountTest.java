30249de3bc7213d48c66b56258167c66a24c67b6
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-04-24 19:04:50 +0300

    Update AmountTest.java

--- android/app/src/main/java/com/example/parking/test/AmountTest.java
+++ android/app/src/main/java/com/example/parking/test/AmountTest.java
@@ -7,10 +7,14 @@ import com.example.parking.util.CurrencyEnum;
 import org.junit.Test;
 import org.testng.Assert;
 
+import java.math.BigDecimal;
+
 public class AmountTest {
     Amount amount;
     @Test
-    public void getAmountTest{
-        amount = new Amount(new Currency(100, CurrencyEnum.EUR),100);
+    public void getAmountTest(){
+        BigDecimal number = new BigDecimal(100);
+        amount = new Amount(new Currency(number, CurrencyEnum.EUR),number);
+        Assert.assertEquals(number,amount.getCurrency());
     }
 }
