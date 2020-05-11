f7b9b11232a76bf91a6eeccf06ba9c952af76644
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-05-11 15:43:57 +0300

    new parking space

---
+++ android/app/src/main/java/com/example/parking/NewParkingSpace.java
@@ -0,0 +1,26 @@
+package com.example.parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class NewParkingSpace extends AppCompatActivity {
+    ArrayList<String> plates = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_parking_space);
+        plates.add("ZXQ2222");
+        plates.add("ZKS2763");
+        Spinner spinner = (Spinner) findViewById(R.id.CarChoice);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(NewParkingSpace.this,
+                android.R.layout.simple_spinner_dropdown_item,plates);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
