c6f2e411ab051260c1e5639d755a153914f54caa
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-05-11 16:58:39 +0300

    update

--- android/app/src/main/java/com/example/parking/HomeScreenActivity.java
+++ android/app/src/main/java/com/example/parking/HomeScreenActivity.java
@@ -2,14 +2,35 @@ package com.example.parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreenActivity extends AppCompatActivity {
-
+    Button parkingSpace;
+    Button request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        parkingSpace = (Button) findViewById(R.id.submit);
+        request = (Button) findViewById(R.id.request);
+        parkingSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomeScreenActivity.this, NewParkingSpace.class);
+                startActivity(myIntent);
+            }
+        });
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomeScreenActivity.this, ViewVehicles.class);
+                startActivity(myIntent);
+            }
+        });
 
     }
 }
--- android/app/src/main/java/com/example/parking/MainActivity.java
+++ android/app/src/main/java/com/example/parking/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent myIntent = new Intent(this, NewParkingSpace.class);
+        Intent myIntent = new Intent(this, HomeScreenActivity.class);
         startActivity(myIntent);
     }
 }
