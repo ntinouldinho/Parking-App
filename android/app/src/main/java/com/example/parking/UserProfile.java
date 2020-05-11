d8be213abb6d2fc115d7b601d6261221e09f018e
petros21 <petrospapa21@gmail.com>
2020-05-11 17:45:22 +0300

    User profile activity made and some work on the code

---
+++ android/app/src/main/java/com/example/parking/UserProfile.java
@@ -0,0 +1,63 @@
+package com.example.parking;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity
+{
+    private EditText fName, lName, email, zipCode, address, phone;
+    private TextView creditsNum;
+    private Button saveBtn, manageCarsBtn, addCreditsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        getUIReferences();
+        fillEditTextsWithSavedData();
+        addClickListeners();
+    }
+
+    private void getUIReferences()
+    {
+        fName = (EditText) findViewById(R.id.firstName);
+        lName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.emailUserProfile);
+        zipCode = (EditText) findViewById(R.id.zipCode);
+        address = (EditText) findViewById(R.id.addressUserProfile);
+        phone = (EditText) findViewById(R.id.phoneUserProfile);
+
+        creditsNum = (TextView) findViewById(R.id.creditsNumUserProfile);
+
+        saveBtn = (Button) findViewById((R.id.saveBtnUserProfile));
+        manageCarsBtn = (Button) findViewById((R.id.manageCarsBtnUserProfile));
+        addCreditsBtn = (Button) findViewById((R.id.addCreditsBtnUserProfile));
+    }
+
+    private void fillEditTextsWithSavedData()
+    {
+        //load the data and fill the texts
+    }
+
+    private void addClickListeners()
+    {
+        saveBtn.setOnClickListener((v) -> {
+            //get the contents of all the editTexts and save them
+        });
+
+        manageCarsBtn.setOnClickListener((v) -> {
+            //change activity to manage cars
+        });
+
+        addCreditsBtn.setOnClickListener((v) -> {
+            // add the specified credits to the total sum
+        });
+    }
+
+}
