96bde8af8c8e16c33c1de2c140b5e67912e83172
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-05-18 17:42:55 +0300

    notification test

--- android/app/src/test/java/com/example/parking/ui/notificationsTest/notificationsPresenterTest.java
+++ android/app/src/test/java/com/example/parking/ui/notificationsTest/notificationsPresenterTest.java
@@ -2,8 +2,12 @@ package com.example.parking.ui.notificationsTest;
 
 import static org.junit.Assert.*;
 
+import com.example.parking.dao.UserDAO;
+import com.example.parking.domain.ParkingRequest;
 import com.example.parking.memorydao.MemoryInitializer;
 import com.example.parking.ui.notifications.notificationsPresenter;
+import com.example.parking.util.Pin;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,7 +23,38 @@ public class notificationsPresenterTest {
     }
 
     @Test
-    public void approveRequest(){
+    public void validateParking(){
+        ParkingRequest request = view.getPinReq().get(0);
+        int requestFunds = request.getParkingSpace().getPrice().getPoints();
 
+        int requestingUserPointsBefore = request.getRequestingUser().getCredits().getPoints();
+
+        int parkedUserPointBefore = request.getParkingSpace().getParkedUser().getCredits().getPoints();
+
+        presenter.validateParking(request,new Pin(4444));
+        assertEquals("Wrong pin.",view.getToast());
+
+        presenter.validateParking(request,new Pin(5555));
+        assertEquals("Transaction complete",view.getToast());
+
+        assertEquals(requestingUserPointsBefore-requestFunds,MemoryInitializer.getUserDAO().find(request.getRequestingUser().getUsername()).getCredits().getPoints());
+
+        assertEquals(parkedUserPointBefore+requestFunds,MemoryInitializer.getUserDAO().find(request.getParkingSpace().getParkedUser().getUsername()).getCredits().getPoints());
+    }
+
+    @Test
+    public void approveRequest(){
+        ParkingRequest request = view.getToApprove().get(0);
+        int pin = presenter.approveRequest(request);
+        assertEquals("Pin generated",view.getToast());
+        assertEquals(pin,MemoryInitializer.getRequestDAO().find(request).getPin().getPin());
+    }
+
+    @Test
+    public void denyRequest(){
+        ParkingRequest request = view.getToApprove().get(0);
+        presenter.denyRequest(request);
+        assertEquals("Request denied",view.getToast());
+        assertNull(MemoryInitializer.getRequestDAO().find(request).getDate());
     }
 }
--- android/app/src/test/java/com/example/parking/ui/notificationsTest/notificationsViewStub.java
+++ android/app/src/test/java/com/example/parking/ui/notificationsTest/notificationsViewStub.java
@@ -7,18 +7,37 @@ import com.example.parking.ui.notifications.notificationView;
 import java.util.ArrayList;
 
 public class notificationsViewStub implements notificationView {
-    String username;
-    ArrayList<ParkingRequest> toApprove = new ArrayList<>();
-    ArrayList<ParkingRequest> toEnterPin = new ArrayList<>();
+    private String username,result;
+    private ArrayList<ParkingRequest> toApprove = new ArrayList<>();
+    private ArrayList<ParkingRequest> toEnterPin = new ArrayList<>();
+
     public notificationsViewStub(){
         username="";
     }
 
     @Override
     public void showNotifications(ArrayList<ParkingRequest> DaoParkingSpace, String notif) {
-        return ;
+        for(ParkingRequest request:DaoParkingSpace){
+             if(request.getParkingSpace().getParkedUser().getUsername().equals(username)){
+                if(request.getPin()!=null) {
+                    toEnterPin.add(request);
+                }else{
+                    if(request.getDate()!=null){
+                        toApprove.add(request);
+                    }
+
+                }
+            }
+        }
     }
 
+    public ArrayList<ParkingRequest> getPinReq(){
+        return toEnterPin;
+    }
+
+    public ArrayList<ParkingRequest> getToApprove(){
+        return toApprove;
+    }
     @Override
     public void enterPinListener(Button myButtons, ParkingRequest reqs) {
 
@@ -35,6 +54,10 @@ public class notificationsViewStub implements notificationView {
 
     @Override
     public void makeToast(String s) {
+        result=s;
+    }
 
+    public String getToast(){
+        return result;
     }
 }
