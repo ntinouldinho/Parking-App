fad683e0a041a22b9c5a2f99e8801db09326535b
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2020-06-03 18:03:24 +0300

    Update ShowParkingPresenter.java

--- android/app/src/main/java/com/example/parking/ui/showParkingSpace/ShowParkingPresenter.java
+++ android/app/src/main/java/com/example/parking/ui/showParkingSpace/ShowParkingPresenter.java
@@ -45,7 +45,7 @@ public class ShowParkingPresenter {
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void add(ParkingSpace parkingSpace){
-        parkingRequestDAO.save(new ParkingRequest(new TimeRange(LocalDateTime.now(),30),new Pin(), userDAO.find(view.getRequestingUser()),parkingSpace));
+        parkingRequestDAO.save(new ParkingRequest(new TimeRange(LocalDateTime.now(),30),null, userDAO.find(view.getRequestingUser()),parkingSpace));
     }
 
 }
