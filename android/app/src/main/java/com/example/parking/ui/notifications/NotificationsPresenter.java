5b22093d48df22d17ac636dbdc06e66dd58e0b18
GerasimosLap <gerasimoslapakis@gmail.com>
2020-06-03 17:29:23 +0300

    redo

---
+++ android/app/src/main/java/com/example/parking/ui/notifications/NotificationsPresenter.java
@@ -0,0 +1,70 @@
+package com.example.parking.ui.notifications;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.parking.dao.ParkingRequestDAO;
+import com.example.parking.dao.UserDAO;
+import com.example.parking.domain.ParkingRequest;
+import com.example.parking.domain.Rating;
+import com.example.parking.memorydao.MemoryInitializer;
+import com.example.parking.util.Pin;
+
+import java.util.ArrayList;
+
+public class NotificationsPresenter {
+    NotificationsView view;
+    ParkingRequestDAO dao;
+    UserDAO users;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public NotificationsPresenter(NotificationsView view, ParkingRequestDAO dao, UserDAO users){
+        this.view=view;
+        this.dao=dao;
+        this.users=users;
+        String username = view.getUserName();
+        ArrayList<ParkingRequest> all = new ArrayList<>();
+
+        for(ParkingRequest request: dao.findAll()){
+            if(request.getRequestingUser().getUsername().equals(username) || request.getParkingSpace().getParkedUser().getUsername().equals(username) ){
+                all.add(request);
+            }
+        }
+        view.showNotifications(all,username);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public boolean validateParking(ParkingRequest request, Pin pin){
+        int results = dao.find(request).validateParking(pin);
+
+
+       if(results==1){
+            dao.delete(request);
+
+           view.makeToast("Transaction complete");
+            return true;
+       }else{
+           view.makeToast("Wrong pin.");
+           return false;
+       }
+    }
+
+    public int approveRequest(ParkingRequest request){
+        int pin=Pin.createPin();
+        request.setPin(new Pin(pin));
+        view.makeToast("Pin generated");
+        return pin;
+    }
+
+    public void denyRequest(ParkingRequest request){
+        request.setDate(null);
+        view.makeToast("Request denied");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createRating(ParkingRequest request){
+        dao.delete(request);
+        Rating rating = new Rating(1,"Not there",request.getParkingSpace().getParkedUser().getUsername(),request.getRequestingUser().getUsername());
+        MemoryInitializer.getRatingDAO().save(rating);
+    }
+}
